04f27e17c72780e5b571cfc3f1a4472549632cc2
Allen Lu <s0904209@gmail.com>
2016-11-16 19:38:49 +0800

    add jquery body color

--- app/TestTwo.js
+++ app/TestTwo.js
@@ -1,11 +1,19 @@
 var React = require('react');
 var ReactRouter = require('react-router');
 var browserHistory = ReactRouter.browserHistory;
+var $ = require('jquery');
 
 var TestTwo = React.createClass({
+
+    componentDidMount: function() {
+        console.log('allen test1');
+        $('body').css('background-color', 'green');
+    },
+
     render() {
+        var styles = require('./TestTwo.css');
         return (
-            <div>
+            <div className={styles.twoblock}>
                 <h1>Hello I am TestTwo Component</h1>
                 <button className="btn btn-success" onClick={browserHistory.goBack}>BACK TO HOME PAGE</button>
             </div>
