95f94da1e98088615bdf91db8f825444f48b106b
Allen Lu <s0904209@gmail.com>
2016-11-22 14:31:20 +0800

    try redux easy example

--- app/main.js
+++ app/main.js
@@ -3,6 +3,21 @@ import ReactDom from "react-dom";
 import TestOne from './TestOne.js';
 var TestTwo = require('./TestTwo.js');
 
+import {createStore} from 'redux';
+
+const reducer = (state, action) => {
+  if (action.type === 'INC') {
+    return state + 1;
+  }
+  return state;
+};
+
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+  console.log('[allen]store changed', store.getState());
+});
+
 var Main = React.createClass({
     getInitialState: function() {
         return {
@@ -14,9 +29,16 @@ var Main = React.createClass({
             switch: !this.state.switch
         });
     },
+    componentWillMount() {
+        store.dispatch({type: 'INC', payload: 1});
+        const myredux = store.getState();
+        console.log('myredux='+myredux);
+    },
     render() {
+        const myredux = store.getState();
         return (
             <div>
+                myredux={myredux}
                 <input type="button" className="btn btn-danger" onClick={this._toggle} value="Press Me!"/>
                 {this.state.switch ? <TestOne /> : <TestTwo />}
             </div>      
